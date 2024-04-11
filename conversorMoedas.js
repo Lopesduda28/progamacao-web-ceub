30f662f6d03bae92a43dc65727f3ffa6e0036393
dudalopes <lopesmel94@gmail.com>
2024-04-11 10:45:23 -0300

    kdkhd

--- conversorMoedas.js
+++ conversorMoedas.js
@@ -15,21 +15,72 @@ let valoresConversao = {
 
 
 
+let valorUsuario = document.getElementById("valor-usuario");
+valorUsuario.addEventListener("keypress", function(event) {
+
+    console.log(event);
+
+    if(event.ctrlKey == true && event.code == "Key")
+
+    if(event.key == "Enter") {
+        converter();
+    }
+
+});
+
+function limpar() {
+    let valorUsuario = document.getElementById("valor-usuario");
+    let resultado = document.getElementById("resultado");
+
+    valorUsuario.value = "";
+    resultado.textCon
+};
+
+
+
 function converter() {
     let valorUsuario = document.getElementById("valor-usuario").value;
 
     let moedaOrigem = document.getElementById("moeda1").value;
     let moedaDestino= document.getElementById("moeda2").value;
 
+    if(valorUsuario == ""){
+        alert("Valor não pode ser vazio!");
+        return;
+    }
+
+    if(moedaOrigem == moedaDestino){
+        alert("As moedas são iguais, não é possível converter!");
+        return;
+    }
+
     let conversao = valorUsuario * valoresConversao[moedaOrigem][moedaDestino];
 
-    let paragrafoResultado = document.getElementById("resultado");
-    paragrafoResultado.textContent = conversao;
+    console.log(conversao);
 
-    console.log(conversao)
-
-    console.log(moedaOrigem);
-    console.log(moedaDestino);
-    console.log(valorUsuario);
+    let simbolo = "";
+    if (moedaDestino == "real") {
+        simbolo = "R$";
+    }
     
+    if (moedaDestino == "dolar") {
+        simbolo = "US$";
+    }
+    
+    if (moedaDestino =="euro") {
+        simbolo = "€";
+    }
+
+    let paragrafoResultado = document.getElementById("resultado");
+    paragrafoResultado.textContent = simbolo + " " + conversao.toFixed(2);
+    
+}
+
+function inverter() {
+
+    let moeda1 = document.getElementById("moeda1").value;
+    let moeda2 = document.getElementById("moeda2").value;
+
+    document.getElementById("moeda1").value = moeda2;
+    document.getElementById("moeda2").value = moeda1;
 }
